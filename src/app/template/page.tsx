4153bc10de83c30e8f5c96559912dd7ddf106082
Muhammad Osama <muhammad96osama@gmail.com>
2025-03-15 04:25:15 +0200

    Initial commit

---
+++ src/app/template/page.tsx
@@ -0,0 +1,16 @@
+const Template = () => {
+  return (
+    <>
+      <h1 className="fs-2xl fc-gradient">no more boring stock</h1>
+      <h2 className="fs-xl fc-text">no more boring stock</h2>
+      <h3 className="fs-lg fc-text">no more boring stock</h3>
+      <h4 className="fs-md fc-text">no more boring stock</h4>
+      <p className="fs-sm fc-text">no more boring stock</p>
+      <p className="fs-sm fc-text-muted">no more boring stock</p>
+      <p className="fs-xs fc-text">no more boring stock</p>
+      <p className="fs-xs fc-text-muted">no more boring stock</p>
+    </>
+  );
+};
+
+export default Template;
